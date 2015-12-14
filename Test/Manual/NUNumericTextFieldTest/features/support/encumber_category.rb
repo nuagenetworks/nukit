e246db44cdfe1ca0403ff41a7acf06a31b1ce0d4
Alexandre Wilhelm <contact.dogild@gmail.com>
2015-12-14 14:57:36 -0800

    Fixed: new version of cucapp and tests

--- Test/Manual/NUNumericTextFieldTest/features/support/encumber_category.rb
+++ Test/Manual/NUNumericTextFieldTest/features/support/encumber_category.rb
@@ -1,22 +1,14 @@
 module Encumber
-  class GUI
 
+  class GUI
     def value_is_equal(xpath, value)
 
-      if value == '""'
+      if !value
         value = ""
       end
 
       result = command 'valueIsEqual', id_for_element(xpath), value
       raise "Value #{value} not found" if result["result"] != "OK"
     end
-
-    def is_control_focused(xpath)
-      result = command "isControlFocused", id_for_element(xpath)
-      raise "Could not find control for element #{xpath}" if result['result'] == "__CUKE_ERROR__"
-      raise "Control #{xpath} is not focused" if result['result'] == "NOT FOCUSED"
-      return result['result'].to_s
-    end
-
   end
 end
