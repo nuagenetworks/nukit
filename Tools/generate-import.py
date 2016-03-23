a4fa6f60514b71c21c56b3e67d3ed72536c4d61d
Antoine Mercadal <antoine.mercadal@gmail.com>
2016-03-23 13:20:05 -0700

    Licensing

--- Tools/generate-import.py
+++ Tools/generate-import.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015, Alcatel-Lucent Inc
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the names of its contributors
+#       may be used to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 from optparse import OptionParser
 
 import os
 import re
-import sys
 import datetime
+
 
 def generate_file(output_file, subfolder):
 
@@ -40,19 +67,8 @@ def generate_file(output_file, subfolder):
     date = datetime.datetime.now()
 
     headers = """/*
-*   Filename:         %s
-*   Created:          %s
-*   Author:           Script
-*   Description:      VSA
-*   Project:          VSD - Nuage - Data Center Service Delivery - IPD
 *
-* Copyright (c) 2011-2012 Alcatel, Alcatel-Lucent, Inc. All Rights Reserved.
-*
-* This source code contains confidential information which is proprietary to Alcatel.
-* No part of its contents may be used, copied, disclosed or conveyed to any party
-* in any manner whatsoever without prior written permission from Alcatel.
-*
-* Alcatel-Lucent is a trademark of Alcatel-Lucent, Inc.
+*   Header
 *
 */""" % (output_file, date.strftime('%a %b %d %H:%M:%S %Z %Y'))
 
@@ -69,10 +85,13 @@ def generate_file(output_file, subfolder):
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-o", "--output", dest="output",
-                        help="Name of the file to generate")
-    parser.add_option("-r", "--recursice", action="store_true", dest="recursive",
-                        help="Import the files find in the subfolder")
+    parser.add_option("-o", "--output",
+                      dest="output",
+                      help="Name of the file to generate")
+    parser.add_option("-r", "--recursice",
+                      action="store_true",
+                      dest="recursive",
+                      help="Import the files find in the subfolder")
 
     options, args = parser.parse_args()
 
